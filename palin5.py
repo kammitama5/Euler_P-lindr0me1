e02dee13d5338eaf57f21cf3439341f15468c79e
kammitama5 <krystal.maughan@gmail.com>
2016-10-17 12:10:01 -0700

    1st commit
    both solutions

---
+++ palin5.py
@@ -0,0 +1,27 @@
+from math import sqrt
+
+
+def Reverse():
+    # since we are looking for 3 digit products, smallest palindrome
+    # we are interested in has to be within range of multiple
+    # of smallest three digit numbers
+
+    for x in xrange(100000, 999999):
+        temp = x;
+        # reverse
+        reversenum = int(str(temp)[::-1]);
+        if temp == reversenum:
+            print temp;
+            # find product of 2 two digit numbers
+            factors = set()
+            for w in range(1, int(sqrt(temp)) + 1):
+                if temp % w == 0:
+                    factors.add(w)
+                    factors.add(temp // w)
+                    if sorted(factors) > 950:
+                        print sorted(factors);
+
+
+Reverse();
+#multiples have to be between 900 and 999 => multiples are 913, 993
+#I hacked my way through this one, but got it right: 906609
